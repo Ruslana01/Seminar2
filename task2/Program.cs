eb62e18577a6b59bcdebd7ce46891bba40964f7b
Руслана <shabaevaruslana@yandex.ru>
2022-10-30 19:54:54 +0300

    Вторая задача(13)

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное число");
+string UserNumber =(Console.ReadLine());
+
+if (UserNumber.Length<3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+Console.WriteLine("Третье число это-"+ UserNumber[2]);
